726e02a2eda71758232b54163484c026f8baccd6
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-09-16 20:01:04 -0600

    Added Features

---
+++ index.js
@@ -0,0 +1,68 @@
+console.log("It Works!");
+
+/*function changeName( event ){
+    let userName = document.querySelector('.name');
+    userName.addEventListener('input', nameInput);
+    function nameInput(element){
+        console.log(element.value);
+        userName.innerText = element.value;
+    }   
+console.log(event);
+}    
+
+
+let profileName = document.querySelector('.editName');
+console.log(profileName);
+
+profileName.addEventListener( 'click', changeName );*/
+
+
+function deleteProfile (event){
+    console.log(event.target);
+    let entireElement = event.target.closest ('li');
+    console.log( entireElement );
+    entireElement.remove();
+}
+
+let accept = document.querySelectorAll('#yes');
+console.log (accept);
+
+let denied = document.querySelectorAll('#no');
+console.log (denied);
+
+for (let i = 0; i < accept.length; i++ ){
+    accept[i].addEventListener("click", deleteProfile);
+}
+
+for (let i = 0; i < denied.length; i++ ){
+    denied[i].addEventListener("click", deleteProfile);
+}
+
+function reduseNum (event){
+    let numLikes = document.querySelector('#menus');
+    console.log(numLikes);
+    let rest = parseInt(numLikes.textContent);
+    console.log(rest); 
+    rest -=1;
+    numLikes.textContent = rest;
+}
+
+for (let i = 0; i < accept.length; i++ ){
+    accept[i].addEventListener("click", reduseNum);
+}
+
+for (let i = 0; i < denied.length; i++ ){
+    denied[i].addEventListener("click", reduseNum);
+}
+
+function addNumber (event){
+    let addNumLikes = document.querySelector('#plus');
+    console.log(addNumLikes);
+    let sum = parseInt(addNumLikes.textContent);
+    sum +=1;
+    addNumLikes.textContent = sum + "+";
+}
+
+for (let i = 0; i < accept.length; i++ ){
+    accept[i].addEventListener("click", addNumber);
+}
